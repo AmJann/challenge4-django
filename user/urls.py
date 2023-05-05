7fb01ff0f38a80cc6488e6effbb488d9a2c19bb0
satkalra <satyam_kalra@yahoo.co.in>
2023-05-05 16:09:40 -0400

    Groups and User
    Groups all URLs tested and working, added id and username for user register and login

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegistrationView.as_view(), name='register'),
     path('check_user_logged_in/', CheckUserLoggedInView.as_view(), name='check_user_logged_in'),
-    path('create_group/', createGroup.as_view(), name='create_group'),
-    path('add_user_to_group/', addUserToGroup.as_view(), name='add_user_to_group')
+    path('create-group/', createGroup.as_view(), name='create_group'),
+    path('add-user-to-group/', addUserToGroup.as_view(), name='add_user_to_group'),
+    path('get-group/', getGroup.as_view(), name = 'Get Group')
 ]
