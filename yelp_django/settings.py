e1aff3c04c3bc0fbb65e1b4d90758e334713f564
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-07-20 11:52:08 -0400

    vercel deploy

--- yelp_django/settings.py
+++ yelp_django/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build', "static")
 
 STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
--- yelp_django/urls.py
+++ yelp_django/urls.py
@@ -19,9 +19,15 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework.authtoken.views import obtain_auth_token
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('user.urls')),
     path('',include('yelp.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
