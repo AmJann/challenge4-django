e2a35422ce54aa17a33b40033e604cc560c7e977
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-07-24 15:13:45 -0400

    changed allowed hosts

--- yelp_django/settings.py
+++ yelp_django/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
